c31bb62999da0966426ed06a34f7e34068aec1ee
AneeshKm <aneeshkmokashi@rediffmail.com>
2023-07-23 23:14:59 +0530

    Added Basic components required to create app

---
+++ Frontend/flashmob-gre-frontend/src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ProgramTable from './components/ProgramTable';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* <h1>Flash Card Sets</h1> */}
+      {/* <ProgramTable /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ Frontend/flashmob-gre-frontend/src/FlashSet/FlashSet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const FlashSet: React.FC = () =>{
+
+    return(
+        <div>Data</div>
+    );
+
+};
+
+export default FlashSet;
