43d61775a8499f8854b1fb5570dff6980845038d
AneeshKm <aneeshkmokashi@rediffmail.com>
2023-07-25 23:01:14 +0530

    added springboot starter skeleton

---
+++ Backend/FlashMobApplication/src/main/java/com/flashmob/FlashMobApplication/FlashMobApplication.java
@@ -0,0 +1,13 @@
+package com.flashmob.FlashMobApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashMobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashMobApplication.class, args);
+	}
+
+}
---
+++ Backend/FlashMobApplication/src/test/java/com/flashmob/FlashMobApplication/FlashMobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashmob.FlashMobApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashMobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
